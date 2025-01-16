985919b9be19e5199956f58261243401e4be7e57
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-16 18:12:31 +0330

    Set up initial backend structure with MongoDB connection and user registration API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,23 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
+import authRoutes from "./routes/auth.routes";
 
 dotenv.config();
 
 const app: Express = express();
-const PORT = process.env.PORT || 5000;
+const PORT: string | number = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes)
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Server is running");
 });
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
